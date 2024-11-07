015e9843e31034969501a360b6ed645328d00c49
Mmachi Ezeh <mmachilawri@gmail.com>
2024-11-07 14:24:22 +0100

    worked with aws and postman

--- backend/settings.py
+++ backend/settings.py
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backend_course',
+        'USER': 'mySuperuser',
+        'PASSWORD': 'mySuperuser',
+        'HOST': 'backendcourse.chuccoam0v02.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
